8eb430e071daf8c6d91d2a8019896a7bfaffd432
Naga Durga Srinivas Adapa <adapasrinivas2004@gmail.com>
2025-08-20 21:43:44 +0530

    Create electionsinchefland.py

---
+++ electionsinchefland.py
@@ -0,0 +1,11 @@
+n=int(input())
+for i in range(n):
+    a,b,c=map(int,input().split())
+    if a>50 :
+        print("A")
+    elif b>50:
+        print("B")
+    elif c>50:
+        print("C")
+    else:
+        print("NOTA")
